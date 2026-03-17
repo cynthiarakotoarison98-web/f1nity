444ece1e12d405a0a367ba4d82f2ca388e0acef2
cynthiarakotoarison98-web <cynthiarakotoarison98@gmail.com>
2026-03-17 22:05:00 +0100

    Fix base href /f1nity/ for GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -150,7 +150,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "473ed1a4ec18f12ea8566caef972d903
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "b96059fd29d1a765623dc192048828b3",
+"flutter_bootstrap.js": "d8f63c08f150ee92ff684668e4cd0ddc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
